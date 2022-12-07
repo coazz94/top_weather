981e4fe11e6ecbf6d8fe41342a832eb9c77535c8
z0047n1j <test@hotmail.com>
2022-12-07 07:53:15 +0100

    chnages

--- script.js
+++ script.js
@@ -4,16 +4,19 @@ const apiKey = "b4e1871079ad0d513a2ab9e3069eb9d0";
 
 
 searchBtn.addEventListener("click", () => {
-    getCordinates(city.value).then((data) => {
-        getWeatherData(data[0], data[1]).then((weatherinfo) => {
-            console.log(weatherinfo.main.temp);
-            // console.log(weatherinfo);
-            console.log(data[2]);
-        });
-    });
+    const a  =  await getCordinates(city.value);
+    console.log(a)
+
+    // getCordinates(city.value).then((data) => {
+    //     getWeatherNow(data.lat, data.lon).then((weatherinfo) => {
+    //         console.log(weatherinfo.main);
+    //     });
+    // });
 });
 
-async function getWeatherData(longitude, latitude) {
+
+
+async function getWeatherNow(longitude, latitude) {
     const weatherFetchLink = `https://api.openweathermap.org/data/2.5/weather?units=metric&lat=${latitude}&lon=${longitude}&appid=`;
     const apiData = await fetch(weatherFetchLink + apiKey);
     const response = await apiData.json();
@@ -24,5 +27,5 @@ async function getCordinates(cityName) {
     const cityCordinatesLink = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=1&appid=`;
     const apiData = await fetch(cityCordinatesLink + apiKey);
     const response = await apiData.json();
-    return [response[0].lat, response[0].lon, response[0].name];
+    return {lat: response[0].lon, lon: response[0].lat};
 }
